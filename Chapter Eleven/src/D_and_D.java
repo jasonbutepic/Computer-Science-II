861a839c72eefd0c2dd2e4a8fa5b537b0cbd916a
jasonbutepic <20250393@nfaschool.org>
2024-01-09 10:06:27 -0500

    fart

--- Chapter Eleven/src/D_and_D.java
+++ Chapter Eleven/src/D_and_D.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 
 public class D_and_D {
     JFrame frame;
-    JPanel movePanel, dungeonBoard, contentPane;
+    JPanel movePanel, dungeonBoard, textPanel, contentPane;
     JButton moveUp, moveRight, moveDown, moveLeft, fire, blank, spaces[][];
     JTextField events;
     JLabel label;
@@ -17,6 +17,7 @@ public class D_and_D {
         contentPane.setLayout(new BorderLayout());
 
         movePanel = new JPanel();
+        movePanel.setBackground(Color.white);
         movePanel.setLayout(new GridLayout(3, 3, 0, 0));
         movePanel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
 
@@ -24,41 +25,50 @@ public class D_and_D {
         blank.setPreferredSize(new Dimension(150, 150));
         blank.setBackground(Color.white);
         blank.setBorderPainted(false);
+        movePanel.add(blank);
 
         moveUp = new JButton("Up");
         moveUp.setPreferredSize(new Dimension(150, 150));
+        movePanel.add(moveUp);
 
         blank = new JButton(" ");
         blank.setPreferredSize(new Dimension(150, 150));
         blank.setBackground(Color.white);
         blank.setBorderPainted(false);
+        movePanel.add(blank);
 
         moveLeft = new JButton("Left");
         moveLeft.setPreferredSize(new Dimension(150, 150));
+        movePanel.add(moveLeft);
 
         blank = new JButton(" ");
         blank.setPreferredSize(new Dimension(150, 150));
         blank.setBackground(Color.white);
         blank.setBorderPainted(false);
+        movePanel.add(blank);
 
         moveRight = new JButton("Right");
         moveRight.setPreferredSize(new Dimension(150, 150));
+        movePanel.add(moveRight);
 
         blank = new JButton(" ");
         blank.setPreferredSize(new Dimension(150, 150));
         blank.setBackground(Color.white);
         blank.setBorderPainted(false);
+        movePanel.add(blank);
 
         moveDown = new JButton("Down");
         moveDown.setPreferredSize(new Dimension(150, 150));
+        movePanel.add(moveDown);
 
         blank = new JButton(" ");
         blank.setPreferredSize(new Dimension(150, 150));
         blank.setBackground(Color.white);
         blank.setBorderPainted(false);
+        movePanel.add(blank);
 
         dungeonBoard = new JPanel();
-        dungeonBoard.setPreferredSize(new Dimension(1000, 1000));
+        dungeonBoard.setPreferredSize(new Dimension(800, 800));
         dungeonBoard.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
         dungeonBoard.setLayout(new GridLayout(10, 10));
         dungeonBoard.setBackground(Color.WHITE);
@@ -74,9 +84,24 @@ public class D_and_D {
             }
         }
 
-        contentPane.add(movePanel, BorderLayout.CENTER);
+        
+
+        textPanel = new JPanel();
+        textPanel.setPreferredSize(new Dimension(400, 100));
+        textPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+        textPanel.setLayout(new GridLayout(1, 1));
+        textPanel.setBackground(Color.WHITE);
+
+        events = new JTextField();
+        events.setEditable(false);
+        events.setBackground(Color.WHITE);
+        events.setBorder(BorderFactory.createLineBorder(Color.black));
+        textPanel.add(events);
+
+        contentPane.add(movePanel, BorderLayout.WEST);
         contentPane.setBackground(Color.white);
         contentPane.add(dungeonBoard);
+        contentPane.add(textPanel, BorderLayout.SOUTH);
         frame.add(contentPane);
         frame.pack();
         frame.setVisible(true);
