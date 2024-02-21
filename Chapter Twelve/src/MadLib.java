9a6181a057b0aac9584bcade9d5930118637dc83
jasonbutepic <20250393@nfaschool.org>
2024-02-21 12:52:52 -0500

    james may

--- Chapter Twelve/src/MadLib.java
+++ Chapter Twelve/src/MadLib.java
@@ -1,27 +1,84 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.Scanner;
-
-//add random function, make story take the entire file, check for # = noun and % = verb (for loop), 
+import java.util.ArrayList;
 
 public class MadLib {
-    static String story;
-    String verb;
-    String noun1;
-    String noun2;
-
     public static void main(String[] args) {
-        
         try {
-            File storyFile = new File("story.txt");
-            Scanner input = new Scanner(storyFile);
-            story = input.nextLine();
+            // Open the story text file
+            File file = new File("I:\\Downloads\\story.txt");
+            Scanner scanner = new Scanner(file);
 
-            while (input.hasNext()) {
-                
+            // Call the getNoun and getVerb methods to use them
+            String noun = getNoun();
+            String verb = getVerb();
+
+            //randomly select a noun and verb from the file
+
+            
+
+            // Replace the # with the noun and % with the verb
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] word = line.split(" ");
+               for (String words : word) {
+                    if (words.contains("#")) {
+                        line = line.replace("#", noun);
+                    }
+                    if (words.contains("%")) {
+                        line = line.replace("%", verb);
+                    }
+               }
+                System.out.println(line);
             }
-        } catch (IOException e) {
-            System.out.println("Problem reading file.");
-            System.err.println("IOException: " + e.getMessage());
+
+            //randomly select a noun and verb from the file
+
+
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
         }
+    }
+   
+
+    public static String getNoun() {
+        //randomly select a noun from the file
+        ArrayList<String> nouns = new ArrayList<String>();
+
+        try {
+            File file = new File("I:\\Downloads\\nouns.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                nouns.add(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return nouns.get((int) (Math.random() * nouns.size()));
+
+    }
+
+    public static String getVerb() {
+        //randomly select a verb from the file
+        ArrayList<String> verbs = new ArrayList<String>();
+
+        try {
+            File file = new File("I:\\Downloads\\verbs.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                verbs.add(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return verbs.get((int) (Math.random() * verbs.size()));
     }
 }
